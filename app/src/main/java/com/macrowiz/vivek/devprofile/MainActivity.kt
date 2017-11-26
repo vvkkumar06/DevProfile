38ac4a0410e67f716f675163165ee0b8d697f038
Vivek <vvkkumar06@gmail.com>
2017-11-26 16:12:15 +0530

    lesson -4

--- app/src/main/java/com/macrowiz/vivek/devprofile/MainActivity.kt
+++ app/src/main/java/com/macrowiz/vivek/devprofile/MainActivity.kt
@@ -14,10 +14,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
-    val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
-       rounded.cornerRadius = 15f
-   logo.setImageDrawable(rounded)
+   // val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+    //val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+      // rounded.cornerRadius = 15f
+   //logo.setImageDrawable(rounded)
 
     }
 }
